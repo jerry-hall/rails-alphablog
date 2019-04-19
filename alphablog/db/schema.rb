8c1a0e67a55af0fa9fd3e03c9a16e9d7ddf5a4e8
Jerry <hall.jerry9@gmail.com>
2019-04-18 18:15:15 -0700

    deleted and re-migrated database

--- alphablog/db/schema.rb
+++ alphablog/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_11_014341) do
+ActiveRecord::Schema.define(version: 2019_04_19_010628) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
