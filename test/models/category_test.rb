fee6a30206f2490441e44fbc7e7d6275b23e55cf
Jerry Hall <jerry.hall@clio.com>
2019-08-14 16:41:25 -0700

    category.rb model tests

--- test/models/category_test.rb
+++ test/models/category_test.rb
@@ -15,4 +15,20 @@ class CategoryTest < ActiveSupport::TestCase
     @category.name = ""
     assert_not @category.valid?
   end
+
+  test "name should be unique" do
+    @category.save
+    category2 = Category.new(name: "sports")
+    assert_not category2.valid?
+  end
+
+  test "name should not be too long" do
+    @category.name = "a" * 26
+    assert_not @category.valid?
+  end
+
+  test "name should not be too short" do
+    @category.name = "a" * 2
+    assert_not @category.valid?
+  end
 end
